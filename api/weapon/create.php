242ffc6d9ef6952b13218e2e9ec0b009938680b2
menouaw <menoua.khttn@gmail.com>
2024-12-04 12:58:53 +0100

    Implement weapon creation endpoint and add validation for mandatory parameters

--- api/weapon/create.php
+++ api/weapon/create.php
@@ -28,3 +28,26 @@ if (validateMandatoryParams($data, ['name', 'health', 'attack', 'defense'])) {
 */
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/dao/weapon.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/utils/server.php';
+
+header('Content-Type: application/json');
+
+if (!methodIsAllowed('create')) {
+    returnError(405, 'Method not allowed');
+    return;
+}
+
+$data = getBody();
+
+if (validateMandatoryParams($data, ['name', 'damage'])) {
+    verifyWeapon($data);
+
+    $newWeaponId = createWeapon($data['name'], $data['damage']);
+    if (!$newWeaponId) {
+        returnError(500, 'Could not create the weapon');
+    }
+    echo json_encode(['id' => $newWeaponId]);
+    http_response_code(201);
+} else {
+    returnError(412, 'Mandatory parameters : name, damage');
+}
