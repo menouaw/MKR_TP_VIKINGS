048f3905c10a4db5bc773ebd2e5bc1e9a312c964
menouaw <menoua.khttn@gmail.com>
2024-12-07 22:32:35 +0100

    Enhance viking CRUD operations to include weapon details and HATEOAS links

--- api/dao/viking.php
+++ api/dao/viking.php
@@ -1,13 +1,51 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/utils/database.php';
+/*
+Vikings 
+Un viking ne peut avoir qu'une seule arme au maximum, mais peut n'en avoir aucune.
+ Modifier la table viking pour qu'elle ait une arme (clé étrangère weaponId vers 
+la table Weapon, peut être null).
+ Ajouter les fonctionnalités CRUD pour les vikings :
+ Mettre à jour les fonctionnalités Read du viking (findOne et findAll) pour qu'elles 
+retournent le détail de l'arme du viking si celui-ci en a une. L'arme doit être 
+retournée au format d'un lien vers le détail de l'arme (HATEOAS) si le viking en a 
+une (sinon renvoyer "weapon": ""). Exemple :
+ Mettre à jour la fonctionnalité Create du viking pour qu'il puisse être créé avec 
+une arme par défaut si elle existe. Retourner une erreur appropriée si elle 
+n'existe pas et ne pas créer le viking.
+ {
+  "id": 1,
+  "name": "Ragnar",
+  "health": 300,
+  "attack": 200,
+  "defense": 150,
+  "weapon": "/weapon/findOne.php?id=3"
+ }
+Mettre à jour la fonctionnalité Update avec PUT pour mettre à jour le viking dans 
+son intégralité. Faire les vérifications appropriées pour mettre à jour l'arme si 
+elle existe, ou la supprimer si elle n'existe pas.
+*/
 
 function findOneViking(string $id) {
     $db = getDatabaseConnection();
-    $sql = "SELECT id, name, health, attack, defense FROM viking WHERE id = :id";
+    $sql = "SELECT id, name, health, attack, defense, weaponId FROM viking WHERE id = :id";
     $stmt = $db->prepare($sql);
     $res = $stmt->execute(['id' => $id]);
     if ($res) {
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        $viking = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $viking['id'] = intval($viking['id']);
+        $viking['health'] = intval($viking['health']);
+        $viking['attack'] = intval($viking['attack']);
+        $viking['defense'] = intval($viking['defense']);
+
+        if ($viking['weaponId']) {
+            $viking['weapon'] = "/weapon/findOne.php?id=" . $viking['weaponId'];
+        } else {
+            $viking['weapon'] = "";
+        }
+        unset($viking['weaponId']);
+        return $viking;
     }
     return null;
 }
@@ -15,16 +53,29 @@ function findOneViking(string $id) {
 function findAllVikings (string $name = "", int $limit = 10, int $offset = 0) {
     $db = getDatabaseConnection();
     $params = [];
-    $sql = "SELECT id, name, health, attack, defense FROM viking";
+    $sql = "SELECT id, name, health, attack, defense, weaponId FROM viking";
     if ($name) {
-        $sql .= " WHERE name LIKE %:name%";
-        $params['name'] = $name;
+        $sql .= " WHERE name LIKE :name";
+        $params['name'] = "%$name%";
     }
     $sql .= " LIMIT $limit OFFSET $offset ";
     $stmt = $db->prepare($sql);
     $res = $stmt->execute($params);
     if ($res) {
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $vikings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($vikings as &$viking) {
+            $viking['id'] = intval($viking['id']);
+            $viking['health'] = intval($viking['health']);
+            $viking['attack'] = intval($viking['attack']);
+            $viking['defense'] = intval($viking['defense']);
+            if ($viking['weaponId']) {
+                $viking['weapon'] = "/weapon/findOne.php?id=" . $viking['weaponId'];
+            } else {
+                $viking['weapon'] = "";
+            }
+            unset($viking['weaponId']);
+        }
+        return $vikings;
     }
     return null;
 }
