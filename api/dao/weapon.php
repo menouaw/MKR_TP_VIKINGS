4e5440c2034ef2bd9f3c2c6a78d85aefd7b2626e
menouaw <menoua.khttn@gmail.com>
2024-12-07 20:44:03 +0100

    Refactor weapon data access: rename functions, update SQL queries, and add weapon retrieval endpoint

--- api/dao/weapon.php
+++ api/dao/weapon.php
@@ -1,13 +1,9 @@
 <?php
-/*
-exemple pour le fichier viking.php
-
-<?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/utils/database.php';
 
-function findOneViking(string $id) {
+function findOneWeapon(string $id) {
     $db = getDatabaseConnection();
-    $sql = "SELECT id, name, health, attack, defense FROM viking WHERE id = :id";
+    $sql = 'SELECT id, type, damage FROM weapon WHERE id = :id';
     $stmt = $db->prepare($sql);
     $res = $stmt->execute(['id' => $id]);
     if ($res) {
@@ -16,13 +12,13 @@ function findOneViking(string $id) {
     return null;
 }
 
-function findAllVikings (string $name = "", int $limit = 10, int $offset = 0) {
+function findAllWeapons (string $type = "", int $limit = 10, int $offset = 0) {
     $db = getDatabaseConnection();
     $params = [];
-    $sql = "SELECT id, name, health, attack, defense FROM viking";
-    if ($name) {
-        $sql .= " WHERE name LIKE %:name%";
-        $params['name'] = $name;
+    $sql = "SELECT id, type, damage FROM weapon";
+    if ($type) {
+        $sql .= " WHERE type LIKE %:type%";
+        $params['type'] = $type;
     }
     $sql .= " LIMIT $limit OFFSET $offset ";
     $stmt = $db->prepare($sql);
@@ -31,57 +27,4 @@ function findAllVikings (string $name = "", int $limit = 10, int $offset = 0) {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     return null;
-}
-
-function createViking(string $name, int $health, int $attack, int $defense) {
-    $db = getDatabaseConnection();
-    $sql = "INSERT INTO viking (name, health, attack, defense) VALUES (:name, :health, :attack, :defense)";
-    $stmt = $db->prepare($sql);
-    $res = $stmt->execute(['name' => $name, 'health' => $health, 'attack' => $attack, 'defense' => $defense]);
-    if ($res) {
-        return $db->lastInsertId();
-    }
-    return null;
-}
-
-function updateViking(string $id, string $name, int $health, int $attack, int $defense) {
-    $db = getDatabaseConnection();
-    $sql = "UPDATE viking SET name = :name, health = :health, attack = :attack, defense = :defense WHERE id = :id";
-    $stmt = $db->prepare($sql);
-    $res = $stmt->execute(['id' => $id, 'name' => $name, 'health' => $health, 'attack' => $attack, 'defense' => $defense]);
-    if ($res) {
-        return $stmt->rowCount();
-    }
-    return null;
-}
-
-function deleteViking(string $id) {
-    $db = getDatabaseConnection();
-    $sql = "DELETE FROM viking WHERE id = :id";
-    $stmt = $db->prepare($sql);
-    $res = $stmt->execute(['id' => $id]);
-    if ($res) {
-        return $stmt->rowCount();
-    }
-    return null;
-}
-*/
-
-require_once $_SERVER['DOCUMENT_ROOT'] . 'MKR_TP_VIKINGS/api/utils/database.php';
-
-function findOneWeapon(string $id) {
-    $db = getDatabaseConnection();
-    $sql = 'SELECT id, name, damage FROM weapon WHERE id = :id';
-    $stmt = $db->prepare($sql);
-    $res = $stmt->execute(['id' => $id]);
-    if ($res) {
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-    return null;
-}
-
-findAllWeapons (string $name = "", int $limit = 10, int $offset = 0) {
-    $db = getDatabaseConnection();
-    $params = [];
-    $sql = "SELECT id, name, damage"
 }
--- api/weapon/find.php
+++ api/weapon/find.php
@@ -1,6 +1,6 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/dao/viking.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/utils/server.php';
+require_once $_SERVER ['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/dao/weapon.php';
+require_once $_SERVER ['DOCUMENT_ROOT'] . '/MKR_TP_VIKINGS/api/utils/server.php';
 
 header('Content-Type: application/json');
 
@@ -9,19 +9,21 @@ if (!methodIsAllowed('read')) {
     return;
 }
 
-$name = '';
+$type = '';
 $limit = 10;
 $offset = 0;
 
-if (isset($_GET['name'])) {
-    $name = trim($_GET['name']);
+if (isset($_GET['type'])) {
+    $type = trim($_GET['type']);
 }
+
 if (isset($_GET['limit'])) {
     $limit = intval($_GET['limit']);
     if ($limit < 1) {
         returnError(400, 'Limit must be a positive and non zero number');
     }
 }
+
 if (isset($_GET['offset'])) {
     $offset = intval($_GET['offset']);
     if ($offset < 0) {
@@ -29,5 +31,5 @@ if (isset($_GET['offset'])) {
     }
 }
 
-$vikings = findAllVikings($name, $limit, $offset);
-echo json_encode($vikings);
+$weapons = findAllWeapons($type, $limit, $offset);
+echo json_encode($weapons);
